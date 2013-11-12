1562be12a085f33ce529f0a6718198ec3cccdbb2
Riordan Panayides <riordan@riordan.eu>
2013-11-12 19:13:55 +0000

    added clipboard support

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ minimal cloudapp uploade
 requirements
 ------------
 + pycloud (https://github.com/originell/pycloudapp)
++ xerox (https://github.com/kennethreitz/xerox)
++ the 'xclip' package
 
 usage
 ------------
-Install pycloud and it's requirements, remove the '.default' part of the config file's name and edit it to reflect your Cloudapp credentials, use the program with ./main.py -u <file to upload>
+Install pycloud and it's requirements, remove the '.default' part of the config file's name and edit it to reflect your Cloudapp credentials, use the program with ./main.py -u <file to upload>. The program wil automatically copy a link for the upload to your clipboard.
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python2
+
 from cloudapp.cloud import Cloud
 from ConfigParser import SafeConfigParser
 import argparse
 import os
+import xerox
 
 argparser = argparse.ArgumentParser(description='Cloudapp Uploader')
 argparser.add_argument('-u', '--upload', help='Filename to upload', required=True)
@@ -13,5 +15,7 @@ confparser.read(os.path.abspath(os.path.join(os.path.dirname(__file__), './small
 mycloud = Cloud()
 mycloud.auth(confparser.get('auth', 'username'), confparser.get('auth', 'password'))
 
-print('Uploading ' + args.upload)
+print('Uploading ' + args.upload + '...')
 mycloud.upload_file(args.upload)
+xerox.copy(mycloud.list_items(page=1, per_page=1)[0]['url'])
+print('Upload Successful. Copied link to clipboard.')
